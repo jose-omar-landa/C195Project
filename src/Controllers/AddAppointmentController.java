c90cb2220f80d38049e1d84c2ca4226b29b2d30b
JoseLanda <jlanda4@wgu.edu>
2023-01-15 12:43:09 -0700

    Added functionality to the cancel button on the Add/Update Apppointment screens

--- src/Controllers/AddAppointmentController.java
+++ src/Controllers/AddAppointmentController.java
@@ -1,7 +1,26 @@
 package Controllers;
 
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class AddAppointmentController{
 
+    public Button cancelButton;
+
+    public void onCancelClicked(ActionEvent actionEvent) {
+        try {
+            Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+            Parent scene = FXMLLoader.load(getClass().getResource("../FXML_Files/AppointmentViewScreen.fxml"));
+            stage.setScene(new Scene(scene));
+            stage.setTitle("Scheduled Appointments");
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
