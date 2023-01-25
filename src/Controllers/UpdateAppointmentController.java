1806d4a502f5c47ad8327617cccc4d463c398a83
JoseLanda <jlanda4@wgu.edu>
2023-01-25 13:02:50 -0700

    update appointment fxml page updated with fxid's for needed textfields/combo boxes

--- src/Controllers/UpdateAppointmentController.java
+++ src/Controllers/UpdateAppointmentController.java
@@ -5,10 +5,26 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class UpdateAppointmentController {
     public Button cancelButton;
+    public ComboBox updateAptContactComboBox;
+    public TextField updateAptIDTextField;
+    public TextField updateAptTitleTextField;
+    public TextField updateAptDescriptionTextField;
+    public TextField updateAptLocationTextField;
+    public TextField updateAptTypeTextField;
+    public DatePicker updateAptStartDate;
+    public DatePicker updateAptEndDate;
+    public Button updateAptSaveButton;
+    public ComboBox updateAptCustomerIdComboBox;
+    public ComboBox updateAptUserIdComboBox;
+    public ComboBox updateAptStartTime;
+    public ComboBox updateAptEndTime;
 
     public void onCancelClicked(ActionEvent actionEvent) {
         try {
@@ -20,5 +36,29 @@ public class UpdateAppointmentController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void onUpdateAptContactComboBoxClicked(ActionEvent actionEvent) {
+    }
+
+    public void onUpdateAptStartDateClicked(ActionEvent actionEvent) {
+    }
+
+    public void onUpdateAptEndDateClicked(ActionEvent actionEvent) {
+    }
+
+    public void onUpdateAptSaveButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void onUpdateAptCustomerIDComboBoxClicked(ActionEvent actionEvent) {
+    }
+
+    public void onUpdateAptUserIDComboBoxClicked(ActionEvent actionEvent) {
+    }
+
+    public void onUpdateAptStartTimeComboClicked(ActionEvent actionEvent) {
+    }
+
+    public void onUpdateAptEndTimeComboClicked(ActionEvent actionEvent) {
     }
 }
