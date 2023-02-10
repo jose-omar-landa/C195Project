ee354360789004a0db08e943c20a9571df893906
JoseLanda <jlanda4@wgu.edu>
2023-02-10 11:01:03 -0700

    created javadocs for Divisions and for Users files

--- src/Objects/Divisions.java
+++ src/Objects/Divisions.java
@@ -1,42 +1,60 @@
 package Objects;
 
+/**This class creates the divisions object for use within the application. */
 public class Divisions {
 
     private int divisionID;
     private String division;
     private int countryID;
 
+    /**This method creates the Divisions object.
+     * @param divisionID provides the division ID.
+     * @param division provides the division name.
+     * @param countryID provides the country name. */
     public Divisions(int divisionID, String division, int countryID) {
-
         this.divisionID = divisionID;
         this.division = division;
         this.countryID = countryID;
     }
 
+    /**This is the getter method for the division ID.
+     * @return returns the division ID number. */
     public int getDivisionID() {
         return divisionID;
     }
 
+    /**This is the setter method for the division ID.
+     * @param divisionID provides the division ID number. */
     public void setDivisionID(int divisionID) {
         this.divisionID = divisionID;
     }
 
+    /**This is the getter method for the division name.
+     * @return returns the division name. */
     public String getDivision() {
         return division;
     }
 
+    /**This is the setter method for the division name.
+     * @param division provides the division name. */
     public void setDivision(String division) {
         this.division = division;
     }
 
+    /**This is the getter method for the country ID.
+     * @return returns the country ID number. */
     public int getCountryID() {
         return countryID;
     }
 
+    /**This is the setter method for the country ID number.
+     * @param countryID provides the country ID number. */
     public void setCountryID(int countryID) {
         this.countryID = countryID;
     }
 
+    /**This method converts the division into a String.
+     * @return returns division as a String. */
     @Override
     public String toString() {
         return division;
--- src/Objects/Users.java
+++ src/Objects/Users.java
@@ -1,43 +1,60 @@
 package Objects;
 
+/**This class creates a Users objects for use within the application. */
 public class Users {
 
     private int userID;
     private String userName;
     private String password;
 
-
+    /**This method creates a Users object.
+     * @param userID provides the user ID.
+     * @param userName provides the username.
+     * @param password provides the password. */
     public Users(int userID, String userName, String password) {
         this.userID = userID;
         this.userName = userName;
         this.password = password;
     }
 
-
+    /**This is the getter method for the user ID.
+     * @return returns the user ID number. */
     public int getUserID() {
         return userID;
     }
 
+    /**This is the setter method for the user ID.
+     * @param userID provides the user ID number. */
     public void setUserID(int userID) {
         this.userID = userID;
     }
 
+    /**This is the getter method for the username.
+     * @return returns the username. */
     public String getUserName() {
         return userName;
     }
 
+    /**This is the setter method for the username.
+     * @param userName provides the username. */
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    /**This is the getter method for the password.
+     * @return returns the password. */
     public String getPassword() {
         return password;
     }
 
+    /**This is the setter method for the password.
+     * @param password provides the password. */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**This method converts the username into a String.
+     * @return returns the username as String. */
     @Override
     public String toString() {
         return userName;
